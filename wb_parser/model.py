b58b443f9468d946dcdeb797dc50a3dd14651f9c
boneyk <124228691+boneyk@users.noreply.github.com>
2025-02-15 16:44:57 +1000

    Добавлены новые поля в модель объекта

--- wb_parser/model.py
+++ wb_parser/model.py
@@ -1,11 +1,7 @@
 from pydantic import BaseModel, field_validator
 from typing import Optional, List, Dict, Any
 
-
-class Price(BaseModel):
-    total: Optional[float] = None
-
-
+# ["id", "название", "бренд", "цена (руб.)", "стоимость доставки", "рейтинг", "количество отзывов", "в наличии","количество картинок","уровень продавца","рейтинг продавца","расстояние до товара","ставка за тысячу показов","участвует в продвижении?", "место товара при продвижении","место товара без продвижения", "количество цветов"]
 class Item(BaseModel):
     id: int
     name: str
@@ -13,7 +9,16 @@ class Item(BaseModel):
     reviewRating: float
     feedbacks: int
     volume: int
+    totalQuantity: int
+    viewFlags: int
+    supplierFlags: int
+    pics: int
+    supplierRating: float
+    dist: int
+    promoTextCard: Optional[str] = None
     sizes: Optional[List[Dict[str, Any]]] = None  # Список размеров, содержащий цену
+    log: Optional[Dict[str, Any]] = None  # Список логов, содержащий маркетинговую и техническую информацию
+    colors: Optional[List[Dict[str, Any]]] = None  # Список цветов товара
 
 
 class Items(BaseModel):
